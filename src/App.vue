d0021b8a21871852310ae1ea5b9794a04f47d552
Joyce <joycelin79@gmail.com>
2023-07-23 17:39:02 -0700

    update deploy config

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ onBeforeMount(async () => {
   // Add entry to tracks array
   for (let record of records) {
     const data = await record.data.json();
-    console.log(data)
+    // console.log(data)
     const track = { 
       record, 
       name: data.name, 
